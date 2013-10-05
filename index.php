76ec1c0f0ed14300bbf0245416c5025a90f272fb
Emma Carrigan <fiquem@cube.netsoc.tcd.ie>
2013-10-05 11:55:04 +0100

    Here is a website.

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<style type="text/css">
+			body
+			{
+				background-color: #ee78ec;
+			}
+			div.info
+			{
+				float: left;
+				width: 500px;
+				height: 500px;
+				background-color: #FF89FD;
+				display: inline;
+			}
+			div.outer
+			{
+				display: table;
+				margin: 0 auto;
+			}
+		</style>
+		<!-- <link rel="stylesheet" type="text/css" href="hack.css"> -->
+	</head>
+	<body>
+		<center>
+		<img style="height: auto; width: 300px;" src="http://spunout.ie/images/SpunOut/Logos/SpunOut_ie.jpg">
+		</center>
+		<div class="outer">
+			<div class="info">
+				Left
+			</div>
+			<div class="info">
+				MAP
+			</div>
+		</div>
+	</body>
+</html>
