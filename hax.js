a7a74ccfcb85ed2791025ee44f11e8256aba4e43
MysteriousDrD <david.fennell.df@gmail.com>
2013-10-05 11:11:58 +0000

    skeleton code for filtering

--- hax.js
+++ hax.js
@@ -1,11 +1,14 @@
-function filter(bounds, loc)
-{
-	
 
-}
 
 function filter2list(f, locArray, bounds)
 {
+
+	function ffilter(bounds, loc)
+	{
+		//hax goes here
 	
+	}
+	var finalLocs = locArray.filter(f)
+	return finalLocs
 
 }
